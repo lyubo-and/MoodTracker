accd9dac7cf661741780e5c4753f1d268966c892
Lyubomir Velikov <lyubo.velikoff@gmail.com>
2022-05-17 15:38:23 +0100

    fix: Android splash screen

--- android/app/src/main/java/com/moodtracker/MainActivity.java
+++ android/app/src/main/java/com/moodtracker/MainActivity.java
@@ -4,11 +4,13 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -18,11 +20,13 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
    */
   @Override
